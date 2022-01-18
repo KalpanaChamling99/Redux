1511542962c4b1691f579c101cb316e0ce68a1eb
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-01-18 19:29:25 +0545

    fix issue occured while using provider

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import store from './Store/index';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
