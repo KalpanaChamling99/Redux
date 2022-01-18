8358f35964cffed1b85c9b9abacd45969a4a3922
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-01-18 19:34:52 +0545

    using redux store in counter components

--- app/src/Components/Counter.js
+++ app/src/Components/Counter.js
@@ -1,12 +1,21 @@
+import { useSelector } from 'react-redux';
 import Card from './../UI/Card';
+import store from './../Store/index';
 import classes from './style.module.scss';
+
 const Counter = () => {
+
+    const counter = useSelector( state => state.counter);
+
+    const toggleCounterHandler = () => {
+
+    }
     return(
         <Card class={classes.counter}>
             <h2 className={classes['counter-title']}>Redux counter</h2>
-            <div className={classes['counter-value']}>Counter Value</div>
+            <div className={classes['counter-value']}>{counter}</div>
             <div className={classes['button-section']}>
-                <button className={classes['default']}>Toggle Counter</button>
+                <button className={classes['default']} onClick={toggleCounterHandler}>Toggle Counter</button>
             </div>
         </Card>
     )
